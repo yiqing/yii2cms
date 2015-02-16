a9c74e87a9c50754092d3a5e12849faef5249fc0
wm00689 <17763768@qq.com>
2015-02-16 09:01:08 +0800

    添加配置文件

---
+++ common/config/main-local.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            //'enableStrictParsing' => true,
+            'rules' => [
+                // ...
+            ],
+        ],
+
+        'redis' => function(){
+            $redis = new redis;
+            $redis->connect('127.0.0.1');
+            return $redis;
+
+        }
+    ],
+];
